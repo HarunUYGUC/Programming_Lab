b3e0368bac5a038205d2f6bf84c59a3be7499ae0
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-24 20:05:18 +0300

    Add project files.

---
+++ Programming_Lab/Basics/Write_Read.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    // internal class Write_Read
+    // olarak otomatik geldi ama öğrendiğime göre aşağıdaki şekilde olmalı.
+    // İki şekilde de doğru çalışıyor.
+    class Write_Read
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            // Öğrendiğime göre Console.ReadLine(); olmalı
+            // Console.WriteLine(); satırından sonra
+            // fakat olmasa da doğru çalışıyor.
+        }
+    }
+}
