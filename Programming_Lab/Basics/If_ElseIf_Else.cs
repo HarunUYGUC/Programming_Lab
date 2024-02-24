5f65c1a588aef389a5ccd76fc3a04a79166a55e9
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-24 20:35:19 +0300

    If - Else If - Else

---
+++ Programming_Lab/Basics/If_ElseIf_Else.cs
@@ -0,0 +1,34 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab.Basics
+{
+    class If_ElseIf_Else
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10.");
+            }
+            else if (number == 20)
+            {
+                Console.WriteLine("Number is 20.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10 or 20.");
+            }
+
+            // Yukarıdaki if-else mantığını bu şekilde tek satırda da yapabiliriz.
+            Console.WriteLine(number == 10 ? "Number is 10." : "Number is not 10.");
+        }
+    }
+}
+*/
