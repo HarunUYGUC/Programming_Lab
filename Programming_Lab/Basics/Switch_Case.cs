d8377f50f7e74da5d2cc428a38cec34fe53895a8
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-24 20:38:40 +0300

    Switch Case

---
+++ Programming_Lab/Basics/Switch_Case.cs
@@ -0,0 +1,31 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    class Switch_Case
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10.");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20.");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20.");
+                    break;
+            }
+        }
+    }
+}
+*/
