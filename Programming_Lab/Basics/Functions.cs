902989e54641fa465f59fe25224412cb82e8d79d
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-24 20:48:59 +0300

    To be continued...

---
+++ Programming_Lab/Basics/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    internal class Functions
+    {
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+
+        static void Main(string[] args)
+        {
+            Add();
+        }
+
+        // "parametre alan ve değer döndüren fonksiyonlar"
+        // ile devam edecek.
+    }
+}
