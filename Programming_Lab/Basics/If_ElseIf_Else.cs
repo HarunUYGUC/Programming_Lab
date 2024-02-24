6894ee2c30359e80264e6a54e5daa87bd72da531
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-24 20:43:05 +0300

    If - Else If - Else

--- Programming_Lab/Basics/If_ElseIf_Else.cs
+++ Programming_Lab/Basics/If_ElseIf_Else.cs
@@ -28,6 +28,22 @@ namespace Programming_Lab.Basics
 
             // Yukarıdaki if-else mantığını bu şekilde tek satırda da yapabiliriz.
             Console.WriteLine(number == 10 ? "Number is 10." : "Number is not 10.");
+
+            
+            // Comparison Operators
+
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0-100.");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200."); ;
+            }
         }
     }
 }
