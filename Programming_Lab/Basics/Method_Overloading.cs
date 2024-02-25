91d882231105a0c06c4724800789037b89f42b0a
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-25 12:06:49 +0300

    Method Overloading

---
+++ Programming_Lab/Basics/Method_Overloading.cs
@@ -0,0 +1,33 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    class Method_Overloading
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 2;
+            int number2 = 10;
+            int number3 = 4;
+
+            Console.WriteLine(Multiply(number1, number2));
+            Console.WriteLine(Multiply(number1, number2, number3));
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+    }
+}
+*/
