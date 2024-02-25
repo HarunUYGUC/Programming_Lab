1ab98f58af00b512c8d501fc9df4d8a3b3a1b95f
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-25 12:36:32 +0300

    Arrays

---
+++ Programming_Lab/Basics/Arrays.cs
@@ -0,0 +1,35 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab.Basics
+{
+    internal class Arrays
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            string[] students2 = { "Kyle Crane", "Leon S. Kennedy", "Jill Valentine" };
+
+            students[0] = "Kyle Crane";
+            students[1] = "Leon S. Kennedy";
+            students[2] = "Jill Valentine";
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("---------------");
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
+*/
