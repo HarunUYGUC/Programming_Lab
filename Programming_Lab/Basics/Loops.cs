4d11c647aacb6381598c8572971c98afddbe2ded
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-25 18:16:55 +0300

    Loops

---
+++ Programming_Lab/Basics/Loops.cs
@@ -0,0 +1,57 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Finished!");
+
+            Console.WriteLine("--------------");
+
+            // While Loop
+            int number = 100;
+
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+
+            Console.WriteLine("--------------");
+
+            // Do-While Loop
+            int number2 = 10;
+
+            do
+            {
+                Console.WriteLine(number2);
+                number2--;
+            } while (number2 >= 0);
+
+            Console.WriteLine("--------------");
+
+            // ForEach Loop
+            string[] students = { "Kyle", "Leon", "Jill" };
+
+            foreach (string student in students)
+            {
+                Console.WriteLine($"Student: {student}");
+            }
+        }
+    }
+}
+*/
