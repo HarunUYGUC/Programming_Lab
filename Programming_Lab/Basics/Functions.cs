2d1f52899297f31c2da10d816c9cb841fb33334d
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-25 11:38:16 +0300

    Functions

--- Programming_Lab/Basics/Functions.cs
+++ Programming_Lab/Basics/Functions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,34 @@ namespace Programming_Lab
 {
     internal class Functions
     {
+        static void Main(string[] args)
+        {
+            Add();
+
+            var result = Add2(20, 30);
+            Console.WriteLine(result);
+
+            // Default parametre kullandık.
+            var result2 = Add3(20);
+            Console.WriteLine(result2);
+        }
         static void Add()
         {
             Console.WriteLine("Added!");
         }
 
-        static void Main(string[] args)
+        static int Add2(int number1, int number2)
         {
-            Add();
+            var result = number1 + number2;
+            return result;
         }
 
-        // "parametre alan ve değer döndüren fonksiyonlar"
-        // ile devam edecek.
+        // Default parametre kullandık.
+        static int Add3(int number1, int number2 = 30)
+        {
+            var result = number1 + number2;
+            return result;
+        }
     }
 }
+*/
