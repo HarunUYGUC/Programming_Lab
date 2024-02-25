d9a7189e1b61900baea1fdded5eb6aeb75624e94
Harun Uyguç <harun.uyguc@hotmail.com>
2024-02-25 18:26:46 +0300

    String Data Type

---
+++ Programming_Lab/Basics/String_Data_Type.cs
@@ -0,0 +1,33 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab
+{
+    class String_Data_Type
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+
+            Console.WriteLine(city);
+            Console.WriteLine(city[0]);
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "İstanbul";
+
+            string result = city + city2;
+            Console.WriteLine(result);
+
+            Console.WriteLine("{0} {1}", city, city2);
+        }
+    }
+}
+*/
