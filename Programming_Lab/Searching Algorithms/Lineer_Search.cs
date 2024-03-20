cf30659596993b2795b8f51d1a2b0fb16ec0c692
Harun Uyguç <harun.uyguc@hotmail.com>
2024-03-20 22:01:14 +0300

    Lineer Search

---
+++ Programming_Lab/Searching Algorithms/Lineer_Search.cs
@@ -0,0 +1,35 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab.Searching_Algorithms
+{
+    internal class Lineer_Search
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 15, 17, -6, -3, 0 };
+            int x = -6;
+            int result = 0;
+            bool flag = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == x)
+                {
+                    result = i;
+                    flag = true;
+                }
+            }
+
+            if (flag == false)
+                Console.WriteLine("Aranan Eleman Bulunamadı.");
+            else
+                Console.WriteLine("Aranan Elemanın Bulunduğu İndis: " + result);
+        }
+    }
+}
+*/
