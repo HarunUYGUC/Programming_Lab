a8db85b908f4c478e4668e07adbf014283975e88
Harun Uyguç <harun.uyguc@hotmail.com>
2024-03-23 13:57:14 +0300

    String

---
+++ Programming_Lab/Others/Strings.cs
@@ -0,0 +1,28 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Programming_Lab.Others
+{
+    internal class Strings
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = {"Leon S. Kennedy","Jill Valentine","Tifa Lockhart",
+                                "Kyle Crane","Ada Wong"};
+
+            for (int i = 0; i < isimler.Length; i++)
+                Console.WriteLine(isimler[i]);
+
+            Console.WriteLine("--------------");
+
+            foreach (string eleman in isimler)
+                Console.WriteLine(eleman);
+        }
+    }
+}
+*/
