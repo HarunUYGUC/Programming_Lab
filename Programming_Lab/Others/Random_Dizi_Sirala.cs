82cf3974cf42f5967b5c72cbe260a46660f65640
Harun Uyguç <harun.uyguc@hotmail.com>
2024-04-18 15:45:26 +0300

    Random Dizi Oluştur ve Sırala

---
+++ Programming_Lab/Others/Random_Dizi_Sirala.cs
@@ -0,0 +1,47 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Lab.Others
+{
+    internal class Random_Dizi_Sirala
+    {
+        static void yazdir(int[] d)
+        {
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.Write(d[i] + ", ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static int[] sirala(int[] d)
+        {
+            Array.Sort(d);
+            return d;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Dizinin kapasitesini giriniz: ");
+            int kapasite = Convert.ToInt32(Console.ReadLine());
+            int[] d = new int[kapasite];
+
+            Random r = new Random();
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = r.Next(10, 101);
+            }
+
+            yazdir(d);
+            int[] sirali = sirala(d);
+            yazdir(sirali);
+        }
+    }
+}
+*/
